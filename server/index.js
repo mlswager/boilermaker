6aded2aff4277e99f09943be788813d84d939235
Mason Swager <mlswager2@gmail.com>
2022-04-13 13:54:16 -0500

    moved some things around, still having issue with mode in webpack

--- server/index.js
+++ server/index.js
@@ -1,32 +1,11 @@
-//npm instal --save express **
-const express = require('express')
-const app = express()
-//npm install --save morgan **logging middleware
-const morgan = require('morgan')
-app.use(morgan('dev'))
+// say our sequelize instance is create in 'db.js'
+const {db} = require('./db'); 
+// and our server that we already created and used as the previous entry point is 'server.js'
+const app = require('./app');
+const port = process.env.PORT || 3000;
 
-// static middleware **request static assets from the public folder (index.html) - connects your back and front-end?
-const path = require ('path')
-app.use(express.static(path.join(__dirname, '../public')))
-
-//npm install --save body-parser
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-
-// matches all requests to /api
-app.use('/api', require('./api')); 
-
-//everything that is not a defined api route goes to the index.html file (the front-end)(because it's a single page application) -- this is also where you can hangle invalid vs valid routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public'))
-})
-
-//the error handler. when things get nexted they end up here
-app.use((err,req,res,next)=>{
-    console.log(err)
-    console.log(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error') // ask about err.message
-})
-
-module.exports = app
+  db.sync()  // sync our database
+    .then(()=>{
+      console.log('DATABASE SYNCED')
+      app.listen(port,()=>console.log(`SERVER RUNNING ON PORT ${port}`)) // then start listening with our express server once we have synced
+    })
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,32 @@
 module.exports = {
-    entry: [
-      './app/index.js'
-    ],
-    output: {
-      path: __dirname,
-      filename: './public/bundle.js'
-    },
-    mode: 'development',
-    devtool: 'source-map',
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-react'
-            ]
-          }
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  //mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-react'
+          ]
         }
-      ]
-    }
+      },
+      {
+          test: /\.css$/,
+          use: [
+              'style-loader',
+              'css-loader',
+          ]
+      }
+    ]
+  }
   }
